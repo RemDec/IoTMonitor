b1e2c11fafaa8ba5e72d5297c95648d35e4737e4
Remy <remydecocq@gmail.com>
2019-04-13 17:40:04 +0200

    ModuleManager ready

--- src/moduleManager.py
+++ src/moduleManager.py
@@ -1,45 +1,77 @@
 from modules.abcModule import *
 from lxml import etree
 from lxml.builder import E
+from utils.misc_fcts import str_param_comp
 
-default_descfile = "../modules/modules_descriptor.xml"
+default_libfile = "../modules/modules_descriptor.xml"
 
 
 class ModManager:
 
-    def __init__(self, desc_file=default_descfile):
-        self.desc_file = desc_file
+    def __init__(self, modlib_file=default_libfile, load_direct=False):
+        self.modlib_file = modlib_file
+        self.available_mods = []
+        if load_direct:
+            self.load_modlib()
 
-    def to_xml_desc(self, mod_instance, keep_params=True):
+    def create_modlib(self, modlist=[], include_nondefault_param=False):
+        tree = etree.ElementTree(E.modlib())
+        for mod_instance in modlist:
+            mod_desc = ModDescriptor(mod_inst=mod_instance, include_nondefault_param=include_nondefault_param)
+            entry = mod_desc.modinfos_to_xml(include_nondefault_param=include_nondefault_param)
+            tree.getroot().append(entry)
+            self.available_mods.append(mod_desc)
+        tree.write(self.modlib_file, pretty_print=True, xml_declaration=True)
+
+    def load_modlib(self):
+        # parse de module library file to build available modules descriptors
+        self.clear_modlib()
+        with open(self.modlib_file, 'r') as f:
+            tree = etree.parse(f)
+        root = tree.getroot()
+        infoslist = root.findall("actmod") + root.findall("passmod")
+        for mod in infoslist:
+            self.available_mods.append(ModDescriptor(xml_tree=mod))
+
+    def add_to_modlib_file(self, mod):
+        # append a module signature on current used module library in modlib_file
+        xml_tree = ModDescriptor(mod_inst=mod).modinfos_to_xml()
+        with open(self.modlib_file, 'a') as f:
+            f.write(etree.tostring(xml_tree).decode())
+
+    def change_modlib(self, new_file):
+        self.modlib_file = new_file
+        self.load_modlib()
+
+    def clear_modlib(self):
+        self.available_mods.clear()
+
+    def get_mod_from_desc(self, id):
+        for mod_desc in self.available_mods:
+            if mod_desc.m_id == id:
+                return mod_desc.get_mod_instance()
+
+    def is_available(self, mod_id, reload_lib=False):
+        # check for presence and dependencies of the given module
+        for mod_desc in self.available_mods:
+            if mod_desc.m_id == mod_id:
+                return True
+        if reload_lib:
+            self.load_modlib()
+            return self.is_available(mod_id)
+        return False
+
+    def modinfos_to_xml(self, mod_instance, keep_params=True):
         # translation to xml general description, doesn't keep any settable param used in the module
-        return ModDescriptor(mod_inst=mod_instance).modinfos_to_xml(include_curr_param=keep_params)
+        return ModDescriptor(mod_inst=mod_instance).modinfos_to_xml(include_nondefault_param=keep_params)
 
-    def curr_params_to_xml(self, mod_instance):
+    def modconfig_to_xml(self, mod_instance, set_id=None):
         # generate xml struct with module proper identifier and used params on it, to reinstance later
-        pass
-
-    def add_to_desc_file(self, mod):
-        # append a module signature on current used library
-        pass
-
-    def mod_desc_from_instance(self, mod_instance):
-        # internal function. Return a mod descriptor easily translatable in different ways to xml
-        pass
-
-    def get_mod_desc(self, id_or_inst):
-        # create a module descriptor from entry in xml with id corresponding or existing module instance
-        if isinstance(id_or_inst, str):
-            pass
-        elif isinstance(id_or_inst, Module):
-            return ModDescriptor(mod_inst=id_or_inst)
+        return ModDescriptor(mod_inst=mod_instance).modconfig_to_xml(set_id)
 
     def get_all_desc(self):
-        # fill a list with all module descriptors of registered modules in desc_file
-        pass
-
-    def is_available(self, mod_id):
-        # check for presence and dependencies of the given module
-        pass
+        # return a list with all module descriptors of registered modules in modlib_file
+        return self.available_mods
 
 
 class ModDescriptor:
@@ -52,15 +84,23 @@ class ModDescriptor:
     #       mod. instance      |  mod. descriptor  |      XML signature/code
     #                       <--|                   |<--|
 
-    def __init__(self, mod_inst=None, xml_signature=None):
-        self.extract_modinfos(mod_inst)
-        self.extract_paraminfos(mod_inst)
-        self.parse_modinfos(xml_signature)
-        self.parse_paraminfos(xml_signature)
+    def __init__(self, mod_inst=None, xml_tree=None, include_nondefault_param=False):
+        self.m_id = "unknown"
+        self.m_active = False
+        self.txt_desc = "No module description provided"
+        self.cmd = "unknown"
+        self.def_timer = -1
+        self.pymod = "unknown python module"
+        self.pyclass = "unknown python class"
+        self.curr_params, self.PARAMS, self.desc_params = ({}, {}, {})
+        if mod_inst is not None:
+            self.extract_modinfos(mod_inst, include_nondefault_param)
+        if xml_tree is not None:
+            self.parse_modinfos(xml_tree, include_nondefault_param)
 
     # ----- From module instance -----
 
-    def extract_modinfos(self, mod_inst):
+    def extract_modinfos(self, mod_inst, include_nondefault_param=False):
         self.m_id = mod_inst.get_module_id()
         self.m_active = mod_inst.is_active()
         self.txt_desc = mod_inst.get_description()
@@ -71,16 +111,22 @@ class ModDescriptor:
             self.def_timer = mod_inst.get_read_interval()
         self.pymod = mod_inst.__module__
         self.pyclass = mod_inst.__class__.__name__
+        self.extract_paraminfos(mod_inst, include_nondefault_param)
 
-    def extract_paraminfos(self, mod_inst):
-        self.curr_params, self.PARAMS, self.desc_params = mod_inst.get_params()
+    def extract_paraminfos(self, mod_inst, include_nondefault_param):
+        all_params = mod_inst.get_params()
+        if include_nondefault_param:
+            self.curr_params, self.PARAMS, self.desc_params = all_params
+        else:
+            self.PARAMS, self.desc_params = all_params[1], all_params[2]
 
     # ----- To XML -----
 
-    def modinfos_to_xml(self, include_curr_param=False):
+    def modinfos_to_xml(self, include_nondefault_param=False):
+        # outputs a XML tree describing the module itself
         modtype = "actmod" if self.m_active else "passmod"
-        modattr = {"id": self.m_id, "pymod": self.pymod, "pyclass": self.pyclass}
-        saved_params = self.curr_param_to_xml() if include_curr_param else E.savedparams()
+        modattr = {"modid": self.m_id, "pymod": self.pymod, "pyclass": self.pyclass}
+        saved_params = self.curr_param_to_xml() if include_nondefault_param else E.savedparams()
         xml = E(modtype,
                     E.desc(self.txt_desc),
                     self.defparam_to_xml(),
@@ -89,11 +135,23 @@ class ModDescriptor:
                 modattr)
         return xml
 
+    def modconfig_to_xml(self, set_id=None):
+        # generate a XML saving module configuration and set identifier (pid or qid)
+        # reference the m_id of the module so it can be reinstancied using a modinfo entry
+        # (which indicates corresponding python module to import, classname, ...)
+        if set_id is None:
+            xml = E.modconfig(modid=self.m_id)
+        else:
+            xml = E.modconfig(modid=self.m_id, setid=set_id)
+        xml.append(self.curr_param_to_xml())
+        return xml
+
     def defparam_to_xml(self):
         params = E.defparams()
         for parname, (defval, mand, prefix) in self.PARAMS.items():
             desc = self.desc_params.get(parname)
-            params.append(E.param(desc, code=parname, mandatory=str(mand), prefix=prefix))
+            attributes = {'code': parname, 'mandatory': str(mand), 'default': defval, 'prefix': prefix}
+            params.append(E.param(desc, attributes))
         return params
 
     def curr_param_to_xml(self):
@@ -102,13 +160,42 @@ class ModDescriptor:
             saved_params.append(E.param(code=parname, value=value))
         return saved_params
 
-    # ----- From XML signature -----
+    # ----- From XML tree -----
 
-    def parse_modinfos(self, xml_sign):
-        pass
+    def parse_modinfos(self, xml_tree, saved_params=True):
+        # xml sign should be element 'actmod' or 'passmod' as returned by modinfos_xml()
+        self.m_id = xml_tree.get("modid")
+        self.pymod = xml_tree.get("pymod")
+        self.pyclass = xml_tree.get("pyclass")
+        self.txt_desc = xml_tree.find("desc").text
+        self.parse_defparam(xml_tree)
+        if saved_params:
+            self.parse_savedparam(xml_tree)
 
-    def parse_paraminfos(self, xml_sign):
-        pass
+    def parse_modconfig(self, xml_tree):
+        # modconfig is a save of parameters for a module, including the unique id of this module and
+        # setid, permitting to reinstanciate it if we have the corresp mod_id class (need pymod pyclass
+        # defined in self in addition to do it with self.get_mod_instance())
+        self.mod_id = xml_tree.get("modid")
+        self.setid = xml_tree.get("setid")
+        self.parse_savedparam(xml_tree)
+
+    def parse_defparam(self, xml_tree):
+        defaults = xml_tree.find("defparams")
+        params = defaults.findall("param")
+        for param in params:
+            code = param.get("code")
+            dflt_paramtuple = (param.get("default"), param.get("mandatory"), param.get("prefix"))
+            self.PARAMS[code] = dflt_paramtuple
+            self.desc_params[code] = param.text
+        return self.PARAMS, self.desc_params
+
+    def parse_savedparam(self, xml_tree):
+        saved = xml_tree.find("savedparams")
+        params = saved.findall("param")
+        for param in params:
+            self.curr_params[param.get("code")] = param.get("value")
+        return self.curr_params
 
     # ----- To module instance -----
 
@@ -122,21 +209,34 @@ class ModDescriptor:
             mod_inst.set_read_interval(self.def_timer)
         return mod_inst
 
+    # ----- Misc -----
+
     def __str__(self):
         modtype = "A" if self.m_active else "P"
+        params_comp = str_param_comp(self.PARAMS, self.curr_params)
         s = f"Module descriptor for [{self.m_id}]({modtype}) def in {self.pymod} by {self.pyclass} class\n"
-        s += f"overlayer for cmd {self.cmd} with params {self.curr_params}"
+        s += f"overlayer for cmd {self.cmd} with considered param setup\n{params_comp}"
         return s
 
 
 if __name__ == '__main__':
     from modules.actives.nmapExplorer import *
     nmap = AModNmapExplorer(params={"IP": "MODIFIED PARAM"})
-    desc = ModDescriptor(mod_inst=nmap)
+    desc = ModDescriptor(mod_inst=nmap, include_nondefault_param=True)
     print(f"Got descriptor :\n{desc}")
     nmap2 = desc.get_mod_instance()
     print(f"Parameters of instance from desc :\n{nmap2.params}")
     nmap2 = desc.get_mod_instance(default_params=False)
     print(f"Parameters of instance from desc keeping curr par :\n{nmap2.params}")
 
-    print(etree.tostring(desc.modinfos_to_xml(True), pretty_print=True).decode())
+    print("## Mod desc infos to XML ##\n", etree.tostring(desc.modinfos_to_xml(True), pretty_print=True).decode())
+    print("## Mod desc config to XML ##\n", etree.tostring(desc.modconfig_to_xml("givenqid"), pretty_print=True).decode())
+
+    from modules.passives.pingTarget import *
+    ping = PModPing()
+    manager = ModManager("test.xml")
+    manager.create_modlib([nmap, nmap2, ping], True)
+    manager.load_modlib()
+    print("## Create and reload modlib ##")
+    for mod in manager.available_mods:
+        print(mod)
--- src/utils/misc_fcts.py
+++ src/utils/misc_fcts.py
@@ -16,6 +16,23 @@ def get_infoname_py(fun):
     return fun.__name__, modname
 
 
+def str_param_comp(defaults, current):
+    """Illustrate which value would be given for each param
+
+    """
+    s = ""
+    for code, (defval, mand, pref) in defaults.items():
+        mand = "T" if mand else "F"
+        s += f"~{code}({mand}): {pref} {defval}"
+        repl = current.get(code)
+        if repl is not None:
+            s += f" but current value {repl}"
+        s += '\n'
+    for other in [code for code in current if defaults.get(code) is None]:
+        s += f"!unref value in default for {other} : {current.get(other)}\n"
+    return s
+
+
 def replace_in_dicts(dic, key, newval):
     """Replace values for a key in multilevel dict
 
@@ -72,3 +89,6 @@ if __name__ == '__main__':
     a = {0: 1, 1: {}, 2: {0: "b", 1: "c"}, 3: {0: {}}}
     replace_in_dicts(a, 0, "a")
     print("\n\nAfter replacing 0 keys with value 'a'\n", a)
+    default = {'par1': ("par1val", True, ""), 'par2': ("par2val", False, ""), 'par3': ("par3val", False, "")}
+    given = {'par1':"new_val1", 'par3': "new_val3", 'unknown': "val"}
+    print("\n\nSee used parameters\n", str_param_comp(default, given))
